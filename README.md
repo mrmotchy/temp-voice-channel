1fd8a0a7c2d1a372b8aba96bed8b3dd46b1f5018
captain motchy <72343164+mrmotchy@users.noreply.github.com>
2021-06-03 13:50:49 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# temp-voice-channel
-a join-to-create bot which can be hostet 24/7 on my discord server
+temp voice channel
---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client } = require("discord.js");
+const keepAlive = require('./server.js');
+ 
+const client = new Client({
+  disableEveryone: true
+});
+ 
+keepAlive();
+client.login(process.env.TOKEN);
+
+const config = require("./config.json");
+client.on("ready", ()=>console.log("READY"));
+
+const jointocreate = require("./jointocreate");
+jointocreate(client);
+
+
+client.login(config.TOKEN);
